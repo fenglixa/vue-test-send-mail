6a3f137a60dbe4f1a65ee72f06e437b6a68aa3d6
Feng Li <fenglixa@cn.ibm.com>
2023-05-14 23:25:14 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,28 @@
 <template>
-    <div class="container">
-        <form>
-          <label>Name</label>
-          <input 
-            type="text" 
-            v-model="name"
-            name="name"
-            placeholder="Your Name"
-          >
-          <label>Email</label>
-          <input 
-            type="email" 
-            v-model="email"
-            name="email"
-            placeholder="Your Email"
-            >
-          <label>Message</label>
-          <textarea 
-            name="message"
-            v-model="message"
-            cols="30" rows="5"
-            placeholder="Message">
-          </textarea>
-          
-          <input type="submit" value="Send" @submit="sendEmail()">
-        </form>
-    </div>
+  <form ref="form" @submit.prevent="sendEmail">
+    <label>Name</label>
+    <input type="text" name="user_name">
+    <label>Email</label>
+    <input type="email" name="user_email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Send">
+  </form>
 </template>
 
 <script>
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 export default {
-  name: 'App',
-  data() {
-    return {
-      name: '',
-      email: '',
-      message: ''
-    }
-  },
   methods: {
-    sendEmail(e) {
-      try {
-        emailjs.sendForm('service_hcqymp3', 'template_s8a4sxn', e.target,
-        'haYaA-viSAy5dOPHt', {
-          name: this.name,
-          email: this.email,
-          message: this.message
-        })
-
-      } catch(error) {
-          console.log({error})
-      }
-      // Reset form field
-      this.name = ''
-      this.email = ''
-      this.message = ''
-    },
+    sendEmail() {
+      emailjs.sendForm('service_j9efpor', 'template_latqkpb', this.$refs.form, 'mqDiUGzzur1Rhn2Sd')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+        }, (error) => {
+            console.log('FAILED...', error.text);
+        });
+    }
   }
- 
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-
-
-<style scoped>
-* {box-sizing: border-box;}
-
-.container {
-  display: block;
-  margin:auto;
-  text-align: center;
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-  width: 50%;
-}
-
-label {
-  float: left;
-}
-
-input[type=text], [type=email], textarea {
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  margin-top: 6px;
-  margin-bottom: 16px;
-  resize: vertical;
-}
-
-input[type=submit] {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-input[type=submit]:hover {
-  background-color: #45a049;
-}
-</style>
