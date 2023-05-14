4f104d8fe3c342cd4814e3ec8278327735337741
fenglixa <fenglixa@cn.ibm.com>
2023-05-15 00:39:48 +0800

    update code

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <form ref="form" @submit.prevent="sendEmail">
-    <label>Name</label>
+    <label>姓名</label>
     <input type="text" name="user_name">
-    <label>Email</label>
+    <label>邮箱</label>
     <input type="email" name="user_email">
-    <label>Message</label>
+    <label>公司</label>
+    <input type="text" name="user_company">
+    <label>你需要什么帮助</label>
     <textarea name="message"></textarea>
     <input type="submit" value="Send">
   </form>
 </template>
 
 <script>
-import emailjs from '@emailjs/browser';
+import emailjs from 'emailjs-com';
 
 export default {
   methods: {
     sendEmail() {
-      emailjs.sendForm('service_j9efpor', 'template_latqkpb', this.$refs.form, 'mqDiUGzzur1Rhn2Sd')
+      emailjs.sendForm('service_j9efpor', 'template_ty92gji', this.$refs.form, 'mqDiUGzzur1Rhn2Sd')
         .then((result) => {
             console.log('SUCCESS!', result.text);
         }, (error) => {
